2079323d653911eaf72965ea935ee8057b452f7c
amine <mohammed-el-amine.djellal@epitech.eu>
2023-01-24 12:01:14 +0100

    [EDIT] connexion completed

--- epi_wiew/src/Componants/Acceuille.js
+++ epi_wiew/src/Componants/Acceuille.js
@@ -1,25 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'; // add , useEffect if u want use it
 import axios from "axios";
 
 const BetaseriesOAuth2 = () => {
   const [url, setUrl] = useState('');
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-    console.log(code);
-  }, []);
+  // useEffect(() => {
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   const code = urlParams.get('code');
+  // }, []);
 
   const handleClick = () => {
+
     const clientId = "5e5b18e63fb7";
     const redirectUri = "http://localhost:3000/Profile";
-    setUrl(`https://www.betaseries.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`);
+    setUrl(`https://www.betaseries.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`)
   }
 
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  console.log(code);
+  const clientId = "5e5b18e63fb7";
+  const clientSecret = "8a63bc9f71009ec837ce294dd27d919e";
+  const redirectUri = "http://localhost:3000/Profile";
+  
+  const postData = () => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+    const data = `client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&code=${code}`;
+    axios.post('https://api.betaseries.com/oauth/access_token', data, config)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  if (code) {
+    postData()
+  }
+  
   return (
     <div>
-      <button onClick={handleClick}>Connecter avec Betaseries</button>
-      {url && <a href={url}>Se connecter</a>}
+      <button onClick={handleClick}>Se connecter avec Betaseries</button>
+      {url && <a href={url}>Clic ici pour poursuivre l'identification</a>}
     </div>
   );
 }
