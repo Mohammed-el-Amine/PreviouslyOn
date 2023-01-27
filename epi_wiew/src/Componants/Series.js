4073e695d94ae5fb4d49357894edc5ed69f6278e
amine <mohammed-el-amine.djellal@epitech.eu>
2023-01-27 13:18:23 +0100

    [EDIT] card views, 3 by lane , almost all content

--- epi_wiew/src/Componants/Series.js
+++ epi_wiew/src/Componants/Series.js
@@ -4,7 +4,8 @@ import Header from './Header';
 
 const Series = () => {
     const [input, setInput] = React.useState("");
-    const [tableHTML, setTableHTML] = React.useState("");
+    const [shows, setShows] = React.useState([]);
+
     const postData = () => {
 
         const clientId = "5e5b18e63fb7";
@@ -14,50 +15,7 @@ const Series = () => {
         axios.get(`https://api.betaseries.com/search/shows?client_id=${clientId}&text=${query}&limit=${limit}`)
             .then(response => {
                 console.log(response.data);
-                console.log(response.data.shows[1].id) // fair un .map pour recuperer tout les id
-                const shows = response.data.shows;
-                const showInfo = shows.map(show => {
-                    return {
-                        following: show.following,
-                        id: show.id,
-                        poster: show.poster,
-                        release_date: show.release_date,
-                        slug: show.slug,
-                        title: show.title
-                    };
-
-                });
-                console.log(showInfo);
-
-                const tableRows = shows.map(show => {
-                    return `
-                        <tr>
-                        <td>${show.following} </td>
-                        <td>${show.id} </td>
-                        <td><img>${show.poster} </img></td>
-                        <td>${show.release_date} </td>
-                        <td>${show.slug} </td>
-                        <td>${show.title} </td>
-                        </tr> `;
-                });
-                const tableHTML = `
-                        <table>
-                          <thead>
-                            <tr>
-                              <th>following</th>
-                              <th>id</th>
-                              <th>poster</th>
-                              <th>release_date</th>
-                              <th>slug</th>
-                              <th>title</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            ${tableRows.join('')}
-                          </tbody>
-                        </table>`;
-                setTableHTML(tableHTML);
-                console.log(tableHTML);
+                setShows(response.data.shows);
             })
             .catch(error => {
                 console.log(error);
@@ -70,8 +28,20 @@ const Series = () => {
             <h1>Welcome to your series page</h1>
             <input type="text" onChange={(e) => setInput(e.target.value)} value={input} id="search"></input>
             <button onClick={postData} id="submitButton">Chercher parmis nos séries votre bonheur</button>
-
-            <div dangerouslySetInnerHTML={{ __html: tableHTML }} />
+            <div className="card-container">
+                {shows.map((show, index) => (
+                    <div key={index} className="card">
+                        <img src={show.poster} alt={show.title} />
+                        <div className="card-content">
+                            <h2>{show.title}</h2>
+                            <p>ID: {show.id}</p>
+                            <p>Following: {show.following ? "Yes" : "No"}</p>
+                            <p>Release Date: {show.release_date}</p>
+                            <p>Slug: {show.slug}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
